0c95bf43a3b7abac9adaf663ef44cded85a2a489
Janet Han <janet.han@gmail.com>
2025-11-04 15:41:18 -0500

    fix static commend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
+// Try to read "owner/repo" from the Actions env, else fallback
+const repo =
+  (process.env.GITHUB_REPOSITORY && process.env.GITHUB_REPOSITORY.split("/")[1]) ||
+  "portfolio"; // <— replace with your repo if you prefer explicit
+
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
